37e6c89856aab7be8455f073081543e5ebc84052
budy6991 <alvmenfer@gmail.com>
2022-05-12 23:18:26 +0300

    Style, variables

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameBoard = {
+    cells : [0,1,2,3,4,5,6,7,8]
+}
+
+const playerOne = (name, mark) => {
+    this.name = name
+    this.mark = mark 
+    return {name, mark}
+  };
+
+
+const playerTwo = (name, mark) => {
+    this.name = name
+    this.mark = mark 
+    return {name, mark}
+  };
