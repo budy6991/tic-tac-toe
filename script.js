949b30ba75b7cffe5d59e7d74592438d79bec620
budy6991 <alvmenfer@gmail.com>
2022-05-13 09:09:23 +0300

    Added play method

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const gameBoard = {
     cells : []
 }
 
-const createPlayer = (name, mark) => {
+const createPlayer = (name,marker) => {
     this.name = name
-    this.mark = mark 
-    return {name, mark}
+    this.marker= marker
+    this.plays = function (){
+        return true
+    }
+    return {name, marker, plays}
   };
 
 const gridContainer = document.querySelector('.grid-container')
@@ -14,7 +17,10 @@ const cellElementsArray = document.querySelectorAll('.cell-element')
 
 gameBoard.cells = cellElementsArray
 
-const playerOne = createPlayer('Juan', 'O')
+const playerOne = createPlayer('Juan', 'O', 'No')
+const playerTwo = createPlayer('Alvaro', 'X', 'No')
+
+playerOne.played()
 
 gameBoard.cells.forEach (cell => 
     cell.addEventListener ('click', function (e){
