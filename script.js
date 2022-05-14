1a720f6e28849290fe2a7af99ffbf777cfe869a4
budy6991 <alvmenfer@gmail.com>
2022-05-14 14:45:56 +0300

    Started OOP from zero

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-const gameBoard = {
-    cells : []
+let Gameboard = {
+
+    Board:[],
+
+    
+
+
+
 }
 
-const createPlayer = (name,marker) => {
-    this.name = name
-    this.marker= marker
-    this.plays = function (){
-        return true
+
+let Player = {
+
+    createPlayer(name,marker, isPlaying) {
+        this.name = name
+        this.marker= marker
+        this.isPlaying = isPlaying
+        return {name,marker,isPlaying}
     }
-    return {name, marker, plays}
-  };
+                
+ };
 
-const gridContainer = document.querySelector('.grid-container')
 
-const cellElementsArray = document.querySelectorAll('.cell-element')
 
-gameBoard.cells = cellElementsArray
 
-const playerOne = createPlayer('Juan', 'O', 'No')
-const playerTwo = createPlayer('Alvaro', 'X', 'No')
 
-playerOne.played()
 
-gameBoard.cells.forEach (cell => 
-    cell.addEventListener ('click', function (e){
-        e.target.textContent = playerOne.mark
-    })
-    )
 
+
+
+//   const gridContainer = document.querySelector('.grid-container')
+  
+//   const cellElementsArray = document.querySelectorAll('.cell-element')
+  
+//   gameBoard.cells = cellElementsArray
+  
+//   const playerOne = createPlayer('Juan', 'O', 'No')
+//   const playerTwo = createPlayer('Alvaro', 'X', 'No')
+  
+  
+//   gameBoard.cells.forEach (cell => 
+//       cell.addEventListener ('click', function (e){
+//           e.target.textContent = playerTwo.marker
+//       })
+//       )
+  
