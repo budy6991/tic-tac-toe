c497a34c04b4362b9a35487baf735dc86209d74d
budy6991 <alvmenfer@gmail.com>
2022-05-14 23:57:54 +0300

    Check for winner added

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ let Player = {
             this.playerTwo.isPlaying = true
             Gameboard.displayMarker(this.playerTwo.marker)
             Gameboard.banCell()
+            Gameboard.checkWinner()
             return
         }        
             this.playerTwo.isPlaying = false
             this.playerOne.isPlaying = true
             Gameboard.displayMarker(this.playerOne.marker)
             Gameboard.banCell()
+            Gameboard.checkWinner()
     }
-    
 };
 
 let Gameboard = {
     
     board: document.querySelectorAll('.cell-element'),
-
     displayMarker(marker){
         this.board.forEach (cell => {
             cell.onclick = function (e){
@@ -48,7 +48,63 @@ let Gameboard = {
     },
 
     checkWinner(){
-        
+        if 
+        ((this.board[0].textContent === 'X'
+        &&this.board[1].textContent === 'X'
+        &&this.board[2].textContent === 'X') 
+        ||(this.board[3].textContent === 'X'
+        &&this.board[4].textContent === 'X'
+        &&this.board[5].textContent === 'X') 
+        ||(this.board[6].textContent === 'X'
+        &&this.board[7].textContent === 'X'
+        &&this.board[8].textContent === 'X')
+        ||(this.board[0].textContent === 'X'
+        &&this.board[3].textContent === 'X'
+        &&this.board[6].textContent === 'X')
+        ||(this.board[1].textContent === 'X'
+        &&this.board[4].textContent === 'X'
+        &&this.board[7].textContent === 'X')
+        ||(this.board[2].textContent === 'X'
+        &&this.board[5].textContent === 'X'
+        &&this.board[8].textContent === 'X')
+        ||(this.board[0].textContent === 'X'
+        &&this.board[4].textContent === 'X'
+        &&this.board[8].textContent === 'X')
+        ||(this.board[2].textContent === 'X'
+        &&this.board[4].textContent === 'X'
+        &&this.board[6].textContent === 'X')) {
+            alert('Player One Won')
+        }
+
+        else if 
+        ((this.board[0].textContent === 'O'
+        &&this.board[1].textContent === 'O'
+        &&this.board[2].textContent === 'O') 
+        ||(this.board[3].textContent === 'O'
+        &&this.board[4].textContent === 'O'
+        &&this.board[5].textContent === 'O') 
+        ||(this.board[6].textContent === 'O'
+        &&this.board[7].textContent === 'O'
+        &&this.board[8].textContent === 'O')
+        ||(this.board[0].textContent === 'O'
+        &&this.board[3].textContent === 'O'
+        &&this.board[6].textContent === 'O')
+        ||(this.board[1].textContent === 'O'
+        &&this.board[4].textContent === 'O'
+        &&this.board[7].textContent === 'O')
+        ||(this.board[2].textContent === 'O'
+        &&this.board[5].textContent === 'O'
+        &&this.board[8].textContent === 'O')
+        ||(this.board[0].textContent === 'O'
+        &&this.board[4].textContent === 'O'
+        &&this.board[8].textContent === 'O')
+        ||(this.board[2].textContent === 'O'
+        &&this.board[4].textContent === 'O'
+        &&this.board[6].textContent === 'O')) {
+            alert('Player Two Won')
+        }
+
+        else {alert('You shat your pants')}
     }
 
 }
